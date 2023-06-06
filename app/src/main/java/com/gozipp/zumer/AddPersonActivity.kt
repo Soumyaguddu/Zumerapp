cc62f6af7c8ba70bc275fb6a1955d3ed140a3bd0
Soumyajit <soumyajit.vawsum@gmail.com>
2023-06-07 01:12:43 +0530

    Initial Commit

--- app/src/main/java/com/gozipp/zumer/AddPersonActivity.kt
+++ app/src/main/java/com/gozipp/zumer/AddPersonActivity.kt
@@ -14,74 +14,27 @@ class AddPersonActivity : BaseActivity() {
         binding = ActivityAddPersonBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setData()
+        binding.imgBack.setOnClickListener {
+            finish()
+        }
 
     }
 
     private fun setData() {
-        val editTextList = listOf(
-            binding.etName,
-            binding.etEmail,
-            binding.etPhone,
-            binding.etDateOfBirth
-        )
 
-
-        for (i in editTextList.indices) {
-            editTextList[i].addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-
-                override fun afterTextChanged(s: Editable?) {
-
-
-                    if (s?.length == 1) {
-                        val nextIndex = (i + 1) % editTextList.size
-                        editTextList[nextIndex].requestFocus()
-                    } else if (s?.length == 0) {
-                        val prevIndex = if (i == 0) editTextList.size - 1 else i - 1
-                        editTextList[prevIndex].requestFocus()
-                    }
-
-                    val allTextFilled = editTextList.all { it.text?.length == 1 }
-                    binding.btnLogin.isSelected = allTextFilled
-
-
-                }
-            })
-        }
 
         binding.etName.afterTextChanged {
 
-            if (binding.etName.text.toString().isNotEmpty()) {
-                binding.profileName.text = binding.etName.text.toString().trim()
-            } else {
-                binding.profileName.text = ""
-            }
+            binding.profileName.text = binding.etName.text.toString().trim()
         }
         binding.etEmail.afterTextChanged {
 
-            if (binding.etEmail.text.toString().isNotEmpty()) {
-                binding.profileEmail.text = binding.etEmail.text.toString().trim()
-            } else {
-                binding.profileEmail.text = ""
-            }
+            binding.profileEmail.text = binding.etEmail.text.toString().trim()
 
         }
         binding.etPhone.afterTextChanged {
 
-            if (binding.etPhone.text.toString().isNotEmpty()) {
-                binding.profilePhone.text = binding.etPhone.text.toString().trim()
-            } else {
-                binding.profilePhone.text = ""
-            }
-
+            binding.profilePhone.text = binding.etPhone.text.toString().trim()
         }
         binding.etDateOfBirth.setOnClickListener {
             val calendar = Calendar.getInstance()
